e0bb781ab712da9c778d8015578a6bd0f9fddfe9
AmberW <ambercrayon@gmail.com>
2016-12-06 21:35:44 -0600

    day 1 part 2 solved

--- AdventOfCode_1/AdventOfCodeDay1/taxicab.cs
+++ AdventOfCode_1/AdventOfCodeDay1/taxicab.cs
@@ -9,23 +9,27 @@ namespace AdventOfCodeDay1
 {
     class taxicab
     {
+        static string input = "../../Data/turns.txt";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Starting");
-
-            //get turns from file
-            string turnsData = GetTurnsFromFile();
-
-            List<string> turns = turnsData.Split(',').Select(p => p.Trim()).ToList(); 
+                 //get turns from file
+             List<string> turns = GetTurnsFromFile().Split(',').Select(p => p.Trim()).ToList(); 
 
             Console.WriteLine("turnscount: " + turns.Count);
 
+            //execute turns, find final coordinate
+            int[] prevCoord = { 0, 0 };
+            int[] coord = { 0, 0 }; //x (horizonal), y (vertical)
+            List<int[]> coordinatesVisited = new List<int[]>();
 
-            //execute turns, find b
-            int[] coordinate = { 0, 0 }; //x (horizonal), y (vertical)
             char direction = 'N';
             int tripLength = 0;
+            int[] secondVisitCoord = { 0, 0 };
+            bool secondVisitFound = false;
             int x = 1;
+
+                
             foreach (string turn in turns){
                
                 string dir = turn.Substring(0, 1);
@@ -44,17 +48,95 @@ namespace AdventOfCodeDay1
 
                 Console.WriteLine("Now Facing: " + direction);
 
-                coordinate = GetNewCoordinate(coordinate, blocks, direction);
-                Console.WriteLine("New Coordinates: X=" + coordinate[0] + " Y=" + coordinate[1]);
+                prevCoord = coord;
+                coord = GetNewCoordinate(coord, blocks, direction);
+                Console.WriteLine("New Coordinates: X=" + coord[0] + " Y=" + coord[1]);
 
+                if (!secondVisitFound)
+                {
+                    //get list of coordinates traveled in this move
+                    List<int[]> visitedThisMove = GetCoordinatesHit(prevCoord, coord);
+
+                    foreach (int[] currentVisit in visitedThisMove)
+                    {
+                        if (!secondVisitFound)
+                        {
+                            if (coordinatesVisited.Exists(c => c[0] == currentVisit[0] && c[1] == currentVisit[1]))
+                            {
+                                secondVisitFound = true;
+                                secondVisitCoord = currentVisit;
+                            }
+                            else
+                            {
+                                coordinatesVisited.Add(currentVisit);
+                            }
+                        }
+                    }
+                }
             }
 
 
             //use taxicab formula to get actual length of trip (number of bocks to Easter Bunny HQ)
-            tripLength = Math.Abs( coordinate[0]) + Math.Abs(coordinate[1]);
+            tripLength = Math.Abs( coord[0]) + Math.Abs(coord[1]);
 
             Console.WriteLine("Actual length of trip: " + tripLength);
 
+            if (secondVisitFound)
+            {
+                int secondVisitTripLength = Math.Abs(secondVisitCoord[0]) + Math.Abs(secondVisitCoord[1]);
+                Console.WriteLine("First Spot Hit Twice: X=" + secondVisitCoord[0] + " Y=" + secondVisitCoord[1]);
+                Console.WriteLine("Length to first coordinate visited twice: " + secondVisitTripLength);
+            }
+        }
+
+        private static List<int[]> GetCoordinatesHit(int[] prevCoord, int[] coord)
+        {
+            List<int[]> visitedThisMove = new List<int[]>();
+
+            //y changed
+            if (prevCoord[0] == coord[0])
+            {
+                //prev y is lower
+                if (prevCoord[1] < coord[1])
+                {
+                    for (int i = prevCoord[1]+1; i <= coord[1]; i++)
+                    {
+                        visitedThisMove.Add(new int[] { prevCoord[0], i });
+                    }
+                }
+                //prev y is higher
+                else
+                {
+                    for (int i = prevCoord[1]-1; i >= coord[1]; i--)
+                    {
+                        visitedThisMove.Add(new int[] { prevCoord[0], i });
+
+                    }
+                }
+            }
+            //x changed
+            else
+            {                
+                //prev x is lower
+                if (prevCoord[0] < coord[0])
+                {
+                    for (int i = prevCoord[0]+1; i <= coord[0]; i++)
+                    {
+                        visitedThisMove.Add(new int[] { i, prevCoord[1] });
+                    }
+                }
+                //prev y is higher
+                else
+                {
+                    for (int i = prevCoord[0]-1; i >= coord[0]; i--)
+                    {
+                        visitedThisMove.Add(new int[] { i, prevCoord[1]});
+
+                    }
+                }
+
+            }
+            return visitedThisMove;
         }
 
         private static int[] GetNewCoordinate(int[] coordinate, int blocks, char direction)
@@ -116,7 +198,7 @@ namespace AdventOfCodeDay1
                 int n = 1;
          
                 // Open the text file using a stream reader.
-                using (StreamReader sr = new StreamReader("../../Data/turns.txt"))
+                using (StreamReader sr = new StreamReader(input))
                 {
                     // Read the stream to a string, and write the string to the console.
                     String line = sr.ReadToEnd();
